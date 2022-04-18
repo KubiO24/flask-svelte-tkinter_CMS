17285b0f65a68a56ee03deb3cef1b2e88d216139
TheBlackIgor <swierczynskigor@gmail.com>
2022-04-18 16:36:31 +0200

    some changes

---
+++ client/src/components/NewsContainer.svelte
@@ -0,0 +1,38 @@
+<script>
+    export let number;
+    number += 1;
+    export let title;
+    export let content;
+    console.log(number, title, content);
+</script>
+
+<div class="newsContainer">
+    <div class="navContainer">News {number}</div>
+    <div class="newsContent">
+        <h3>{title}</h3>
+        <p>{content}</p>
+        <button>Go somewhere</button>
+    </div>
+</div>
+
+<style>
+    .newsContainer {
+        max-width: 500px;
+        min-width: 300px;
+        width: 25%;
+        display: flex;
+        flex-direction: column;
+        border-radius: 30px;
+        overflow: hidden;
+        border: 1px solid #c6e4ff;
+        margin: 20px;
+    }
+    .navContainer {
+        padding: 10px;
+        background-color: #c6e4ff;
+        text-align: center;
+    }
+    .newsContent {
+        padding: 0 10px 10px 10px;
+    }
+</style>
--- client/src/routes/Main.svelte
+++ client/src/routes/Main.svelte
@@ -1,8 +1,94 @@
 <script>
     import Header from "../components/header.svelte";
+    import NewsContainer from "../components/NewsContainer.svelte";
 
-    let slider = true;
+    let showSlider = true;
+    let showNews = true;
+    let someSec = true;
+
+    const newsContent = [
+        {
+            title: "Special title treatment",
+            info: "With supporting text below as a natural lead-in to additional content.",
+        },
+        {
+            title: "Special title treatment",
+            info: "With supporting text below as a natural lead-in to additional content.",
+        },
+        {
+            title: "Special title treatment",
+            info: "With supporting text below as a natural lead-in to additional content.",
+        },
+    ];
 </script>
 
 <Header />
-{#if slider}{/if}
+
+{#if showSlider}
+    <div class="slider">
+        <p>Slider</p>
+    </div>
+{/if}
+{#if showNews}
+    <div class="newsContainer">
+        {#each newsContent as item, i}
+            <NewsContainer number={i} title={item.title} content={item.info} />
+        {/each}
+    </div>
+{/if}
+
+{#if someSec}
+    <div class="someSec">
+        <div class="left">
+            <div>
+                <h2>First featurette heading.</h2>
+                <h3>It will blow your mind</h3>
+            </div>
+            <div>
+                Some great placeholder content for the first featurette here.
+                Imagine some exciting prose here.
+            </div>
+        </div>
+        <div class="right">
+            <div>500 x 500</div>
+        </div>
+    </div>
+{/if}
+
+<style>
+    .slider {
+        width: 100%;
+        height: 500px;
+        background-color: grey;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+    .slider p {
+        font-size: 100px;
+        margin: 0;
+        padding: 0;
+    }
+    .newsContainer {
+        width: 100%;
+        margin: 30px 0;
+        height: auto;
+        display: flex;
+        justify-content: space-around;
+        align-items: center;
+        flex-wrap: wrap;
+        flex-direction: row;
+    }
+    .someSec {
+        width: 80%;
+        min-width: 320px;
+        display: flex;
+        flex-direction: row;
+        justify-content: space-around;
+        align-items: center;
+    }
+    .right div {
+        width: 500px;
+        height: 500px;
+    }
+</style>
