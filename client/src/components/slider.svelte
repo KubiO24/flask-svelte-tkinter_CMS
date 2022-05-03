a8f63daa072ea7f388e5d5b77e75e7cf72654d08
TheBlackIgor <swierczynskigor@gmail.com>
2022-05-03 18:09:49 +0200

    svelte-carousel

--- client/src/components/slider.svelte
+++ client/src/components/slider.svelte
@@ -1,53 +1,49 @@
 <script>
+    import Carousel from "svelte-carousel";
+
+    let carousel; // for calling methods of the carousel instance
     export let data;
 
-    const slides = [];
     const delay = data.sliderDuration;
-    let currentSlide = 0;
-    let interval;
-
-    data.sliderItems.forEach((item, i) => {
-        slides.push("slide" + i);
-    });
-
-    window.onload = () => {
-        interval = setInterval(changeSlide, delay);
-    };
-    const changeSlide = () => {
-        const lastSlide = currentSlide;
-        currentSlide++;
-        if (currentSlide === slides.length) currentSlide = 0;
-        console.log(lastSlide, currentSlide);
-
-        document.getElementById(slides[lastSlide]).classList.remove("show");
-        document.getElementById(slides[lastSlide]).classList.add("hide");
-        document.getElementById(slides[currentSlide]).classList.remove("hide");
-        document.getElementById(slides[currentSlide]).classList.add("show");
-    };
 </script>
 
 {#if data.sliderItems.length > 0}
     <div class="sliderMain" style="color:{data.sliderColor};">
-        {#each data.sliderItems as item, i}
-            {#if i === 0}
-                <div class="slider show" id={`slide${i}`}>
+        <Carousel
+            bind:this={carousel}
+            autoplay={true}
+            autoplayDuration={delay}
+            duration={800}
+            autoplayProgressVisible={true}
+            pauseOnFocus={true}
+        >
+            {#each data.sliderItems as item}
+                <!-- <div
+                    class="slider"
+                    style="background-image: url({item.sliderPhoto});"
+                >
+                    <p>{item.sliderText}</p>
+                </div> -->
+                <div class="slider">
                     <p>{item.sliderText}</p>
                 </div>
-            {:else}
-                <div class="slider hide" id={`slide${i}`}>
-                    <p>{item.sliderText}</p>
-                </div>
-            {/if}
-        {/each}
+            {/each}
+        </Carousel>
     </div>
 {/if}
 
 <style>
     .sliderMain {
         height: 500px;
+        -webkit-touch-callout: none; /* iOS Safari */
+        -webkit-user-select: none; /* Safari */
+        -khtml-user-select: none; /* Konqueror HTML */
+        -moz-user-select: none; /* Old versions of Firefox */
+        -ms-user-select: none; /* Internet Explorer/Edge */
+        user-select: none; /* Non-prefixed version, currently
+                                  supported by Chrome, Edge, Opera and Firefox */
     }
     .slider {
-        position: absolute;
         width: 100%;
         height: 500px;
         background-color: grey;
@@ -55,12 +51,6 @@
         justify-content: center;
         align-items: center;
         transition: all ease 1s;
-    }
-    .show {
-        opacity: 1;
-    }
-    .hide {
-        opacity: 0;
     }
     .slider p {
         font-size: 100px;
