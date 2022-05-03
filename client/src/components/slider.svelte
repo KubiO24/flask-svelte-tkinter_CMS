115d69655d4a15b53b15c185a60d177380090edd
TheBlackIgor <swierczynskigor@gmail.com>
2022-05-03 12:38:06 +0200

    slider nie działa

--- client/src/components/slider.svelte
+++ client/src/components/slider.svelte
@@ -1,21 +1,63 @@
 <script>
     export let data;
 
-    let imgs = data.sliderItems;
+    const slides = [];
+    const delay = data.sliderDuration;
+    let currentSlide = 0;
+
+    data.sliderItems.forEach((item, i) => {
+        slides.push("slide" + i);
+    });
+
+    window.onload = () => {
+        setInterval(changeSlide, delay);
+    };
+    const changeSlide = () => {
+        const lastSlide = currentSlide;
+        currentSlide++;
+        if (currentSlide === slides.length) currentSlide = 0;
+        console.log(lastSlide, currentSlide);
+
+        document.getElementById(slides[lastSlide]).classList = "slider hide";
+        document.getElementById(slides[currentSlide]).classList = "slider show";
+    };
 </script>
 
-<div class="slider">
-    <p>Slider</p>
-</div>
+{#if data.sliderItems.length > 0}
+    <div class="sliderMain" style="color:{data.sliderColor};">
+        {#each data.sliderItems as item, i}
+            {#if i === 0}
+                <div class="slider show" id={`slide${i}`}>
+                    <p>{item.sliderText}</p>
+                </div>
+            {:else}
+                <div class="slider hide" id={`slide${i}`}>
+                    <p>{item.sliderText}</p>
+                </div>
+            {/if}
+        {/each}
+    </div>
+{/if}
 
 <style>
+    .sliderMain {
+        height: 500px;
+    }
     .slider {
+        position: absolute;
         width: 100%;
         height: 500px;
         background-color: grey;
         display: flex;
         justify-content: center;
         align-items: center;
+        transition: all ease 1s;
+    }
+    .show {
+        opacity: 1;
+    }
+    .hide {
+        opacity: 0;
     }
     .slider p {
         font-size: 100px;
